dcb81d306695d982c2442ec2f45a4a7f78228acb
xiangpeng <zhangxiangpeng2008@gmail.com>
2019-11-16 14:18:01 +0800

    add button to cards, pass, fold

--- holdem.py
+++ holdem.py
@@ -36,9 +36,6 @@ holdem_form = html.Div( [
         dcc.Dropdown(
             id='input_option',
             options=[
-                {'label': 'cards', 'value': 'cards[`]'},
-                {'label': 'pass', 'value': 'pass[`]'},
-                {'label': 'fold', 'value': 'fold[`]'},
                 {'label': 'new game', 'value': 'newGame[`]'},
                 {'label': 'show people on this table', 'value': 'people_on_table`'},#玩家们
                 {'label': 'join', 'value': 'join`'},#加入
@@ -48,11 +45,13 @@ holdem_form = html.Div( [
             style={'fontSize': fontSize},
             className='fullScreen',
             ),
+        html.Br(),
         dcc.Input(id='input_cmd', type='text', placeholder='Enter your text here.',value='', debounce=True, className='input_cmd', style={'fontSize': fontSize},),
-        html.Button(id='submit_button', n_clicks=0, children='Submit', className='button', style={'fontSize': fontSize}),
-        html.Button(id='submit_cards' , n_clicks=0, children='Submit', className='button', style={'fontSize': fontSize}),
-        html.Button(id='submit_pass'  , n_clicks=0, children='Submit', className='button', style={'fontSize': fontSize}),
-        html.Button(id='submit_fold'  , n_clicks=0, children='Submit', className='button', style={'fontSize': fontSize}),
+        html.Button(id='submit_button', n_clicks_timestamp=0, n_clicks=0, children='Submit', className='button', style={'fontSize': fontSize}),
+        html.Br(),
+        html.Button(id='submit_cards' , n_clicks_timestamp=0, n_clicks=0, children='Cards', className='smallButton', style={'fontSize': fontSize}),
+        html.Button(id='submit_pass',   n_clicks_timestamp=0, n_clicks=0, children='Pass',  className='smallButton', style={'fontSize': fontSize}),
+        html.Button(id='submit_fold'  , n_clicks_timestamp=0, n_clicks=0, children='Fold',  className='smallButton', style={'fontSize': fontSize}),
         ]),
     html.Div(id='cache_history', style={'display': 'none'}, children="[\"Interaction history\"]"),
     html.Div(id='history'),
@@ -117,7 +116,6 @@ def getQuery(command, existe_value, isPoker = False):
     print('python command: '+pythonCommand)
     qres=q.sendSync(pythonCommand)
     if isPoker:
-        # qres='\n'.join(['# _'+''.join(cards52[index])+'_' for index in qres])
         qres='\n'.join(['# '+''.join(cards52[index]) for index in qres])
     elif isinstance(qres, pd.core.frame.DataFrame):
         str_df = qres.select_dtypes([np.object])
@@ -134,32 +132,29 @@ def getQuery(command, existe_value, isPoker = False):
 
 @app.callback(
         [ Output('cache_history', 'children'), Output('input_option', 'value'),Output('input_cmd', 'value') ], 
-        [ Input('submit_button','n_clicks')],
+        [ Input('submit_button','n_clicks_timestamp'), Input('submit_cards','n_clicks_timestamp'), Input('submit_pass','n_clicks_timestamp'), Input('submit_fold','n_clicks_timestamp'),],
         [
             State('input_option', 'value'),
             State('input_cmd', 'value'),
             State('cache_history', 'children')
             ]
         )
-def update_button(click, input_opt, input_cmd, existe_value):
-    if not input_opt:
+def update_output_div(tSubmit, tCards, tPass, tFold, input_opt, input_cmd, existe_value):
+    tClicks = [tSubmit, tCards, tPass, tFold]
+    lastClicked = tClicks.index(max(tClicks))
+    if (tClicks[lastClicked] == 0) or ((not input_opt) and (lastClicked == 0)):
         return [existe_value,'','']
-    query = getQuery(input_opt+('[\\"'+input_cmd.replace(" ", "")+'\\"]' if input_cmd else ''), existe_value, input_opt=='cards[`]')
-    return query,'',''
-
-@app.callback(
-        [ Output('cache_history', 'children'), Output('input_option', 'value'),Output('input_cmd', 'value') ], 
-        [ Input('submit_button','n_clicks')],
-        [
-            State('input_option', 'value'),
-            State('input_cmd', 'value'),
-            State('cache_history', 'children')
-            ]
-        )
-def update_output_div(click, input_opt, input_cmd, existe_value):
-    if not input_opt:
-        return [existe_value,'','']
-    query = getQuery(input_opt+('[\\"'+input_cmd.replace(" ", "")+'\\"]' if input_cmd else ''), existe_value, input_opt=='cards[`]')
+    query = ''
+    print(tClicks)
+    print(lastClicked)
+    if lastClicked == 0:
+        query = getQuery(input_opt+('[\\"'+input_cmd.replace(" ", "")+'\\"]' if input_cmd else ''), existe_value, input_opt=='cards[`]')
+    elif lastClicked == 1:
+        query = getQuery('cards[`]', existe_value, True)
+    elif lastClicked == 2:
+        query = getQuery('pass[`]', existe_value)
+    elif lastClicked == 3:
+        query = getQuery('fold[`]', existe_value)
     return query,'',''
 
 @app.callback(
@@ -171,7 +166,6 @@ def update_output_div(existe_value):
     return dcc.Markdown(res)
 
 server = app.server
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
