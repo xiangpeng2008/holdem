ec68a89df0484a8c68028e4484ea29d54d56e05d
xiangpeng <zhangxiangpeng2008@gmail.com>
2019-11-17 20:13:09 +0800

    update .gitignore

--- run.sh
+++ run.sh
@@ -4,9 +4,7 @@ mkdir -p log
 currentTime="`date +%Y%m%d_%H%M%S`"
 kill $(ps aux | grep '7778 holdem' | awk '{print $2}')
 kill $(ps aux | grep 'server.cpp.o 20002' | awk '{print $2}')
-#nohup runcpp -c server.cpp 20002 &> log/cpp_$currentTime.txt &
 nohup ~/Documents/Tools/bash/runcpp -c server.cpp 20002 &> text.txt &
-#runcpp -c server.cpp 20002
 sleep 3
 source ~/venv37/bin/activate
 nohup gunicorn --bind 0.0.0.0:7778 holdem:server &> log/dash_$currentTime.txt &
